4517625ba29d6e75b33347b0937d97e05cc18261
“sunnysingh1693” <ssingh@kernagency.com>
2022-08-16 19:26:37 +0530

    added redux

--- src/store.js
+++ src/store.js
@@ -1,8 +1,24 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { todos } from "./todos/reducers";
 
 const reducers = { todos };
 
-const rootReducers = combineReducers(reducers);
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
-export const configureStore = () => createStore(rootReducers);
+const rootReducers = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
